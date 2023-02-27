680e2713ec23118296ce932a569abdf78d25a932
RafiulOmar2022 <111574212+Rafiul-Omar2022@users.noreply.github.com>
2023-02-27 19:40:35 +0600

    boiler plate of HTML

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,7 +8,7 @@ int main(){
     const char MARKUP_TAG[] = {'<', '>', '\\'};
 
     const string MARKUP_NAME_START[] = {
-         "<html>", "<head>", "<title>", "<body>", "<h1>","<h2>", "<h3>", "<h4>", "<h5>", "<h6>", "<p>",
+         "<html>", "<head>", "<title>", "<body>", "<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>", "<p>",
          "<div>", "<em>", "<strong>", "<u>"
          };
 
@@ -21,16 +22,36 @@ int main(){
 
 
 
+
+    string title = "",
+            body = ""; // code injector
+
+
+
     string template_html = MARKUP_NAME_WITHOUT_END_TAG[0] + "\n"+
-                           MARKUP_NAME_START[0]+ "\n" +
-                           MARKUP_NAME_START[1]+ "\n" +
-                           MARKUP_NAME_WITHOUT_END_TAG[1] + "\n" +
-                           MARKUP_NAME_START[2] + "Document" + MARKUP_NAME_END[2] +"\n" +
-                           MARKUP_NAME_END[1] + "\n" +
-                           MARKUP_NAME_START[3] + "\n" + MARKUP_NAME_END[3] + "\n" +
+                           MARKUP_NAME_START[0]+ "\n\n" +
+                           MARKUP_NAME_START[1]+ "\n\t" +
+                           MARKUP_NAME_WITHOUT_END_TAG[1] + "\n\t" +
+                           MARKUP_NAME_START[2] + title + MARKUP_NAME_END[2] +"\n" +
+                           MARKUP_NAME_END[1] + "\n\n" +
+                           MARKUP_NAME_START[3] + "\n\t" +body+"\n"+ MARKUP_NAME_END[3] + "\n\n" +
                            MARKUP_NAME_END[0];
 
     cout<<template_html;
+
+
+    fstream html_file;
+
+    html_file.open("index.html", ios::out);
+
+    if(!html_file){
+        cout<<"File creation failed."<< endl;
+    }
+    else {
+        html_file<<template_html;
+        html_file.close();
+    }
+
 
 
     return 0;
